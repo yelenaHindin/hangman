90e924a205c386f44d77445a03ab1b5aeabb7a16
Yelena Gindin <yelenar@gmail.com>
2017-01-07 19:39:41 +0200

    More tests

--- tests/hangmanTests.js
+++ tests/hangmanTests.js
@@ -12,7 +12,47 @@ describe('Test bundle', function() {
     it('Get word', function() {
         var h = new Hangman.Hangman(words, 1);
         h.word().should.be.equal(words[1]);
+        h.nFailedGuesses().should.be.equal(0);
+        h.state().should.be.empty();
     });
 
+    it('One correct step e', function() {
+        var h = new Hangman.Hangman(["tree"], 0);
+        h.word().should.be.equal("tree");
+
+        h.nFailedGuesses().should.be.equal(0);
+        h.state().should.be.empty();
+
+        h.guess("e");
+
+        h.nFailedGuesses().should.be.equal(0);
+        h.state().should.be.eql([ , , true, true]);
+    });
+
+    it('One correct step t', function() {
+        var h = new Hangman.Hangman(["tree"], 0);
+        h.word().should.be.equal("tree");
+
+        h.nFailedGuesses().should.be.equal(0);
+        h.state().should.be.empty();
+
+        h.guess("t");
+
+        h.nFailedGuesses().should.be.equal(0);
+        h.state().should.be.eql([true]);
+    });
+
+    it('One incorrect step z', function() {
+        var h = new Hangman.Hangman(["tree"], 0);
+        h.word().should.be.equal("tree");
+
+        h.nFailedGuesses().should.be.equal(0);
+        h.state().should.be.empty();
+
+        h.guess("z");
+
+        h.nFailedGuesses().should.be.equal(1);
+        h.state().should.be.eql([]);
+    });
 
 });
